6d7280610b0c67b8b473ebaa65d8018a2883327f
rushitazignuts <rushitav@zignuts.com>
2024-07-18 16:43:11 +0530

    add components folder in cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    experimentalStudio: true,
     projectId: "45a4qx",
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173'
   },
   component: {
     projectId: "45a4qx",
-    specPattern: ['src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/component/**/*.{js,jsx,ts,tsx}'],
+    specPattern: ['src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/components/**/*.{js,jsx,ts,tsx}'],
     devServer: {
       framework: 'vue',
       bundler: 'vite'
--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -4,5 +4,11 @@ describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'You did it!')
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.green').click();
+    cy.get('[href="/about"]').click();
+    cy.get('[href="/"]').click();
+    cy.get('.router-link-active').click();
+    /* ==== End Cypress Studio ==== */
   })
 })
