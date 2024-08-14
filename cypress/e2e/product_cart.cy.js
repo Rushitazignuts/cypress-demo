967f6fc2e172a920a7ac0694e58eec105bec7f70
rushitazignuts <rushitav@zignuts.com>
2024-08-14 15:46:07 +0530

    update product e2e file

--- cypress/e2e/product_cart.cy.js
+++ cypress/e2e/product_cart.cy.js
@@ -29,9 +29,58 @@
   // });
 
 
-  // cypress/e2e/product-list.cy.js
 
 // cypress/e2e/product_cart.cy.js
+
+// describe('Product List', () => {
+//   beforeEach(() => {
+//     // Visit the page where your component is rendered
+//     cy.visit('/');
+//   });
+
+//   it('should display the initial products', () => {
+//     cy.get('.product-list li').should('have.length', 1);
+//     cy.get('.product-list li').first().contains('Product A 150');
+//   });
+
+//   it('should add a new product to the cart', () => {
+//     cy.get('.input').type('Product B');
+//     cy.get('button').contains('Add').click();
+
+//     cy.get('.product-list li').should('have.length', 2);
+//     cy.get('.product-list li').last().contains('Product B 100');
+//   });
+
+//   it('should update the cart total correctly', () => {
+//     cy.get('.input').type('Product B');
+//     cy.get('button').contains('Add').click();
+
+//     // Wait for the cart total to update
+//     cy.get('.cart h3').should('contain', 'Cart Total: 250');
+//   });
+
+//   it('should remove a product from the cart', () => {
+//     cy.get('.input').type('Product B');
+//     cy.get('button').contains('Add').click();
+
+//     cy.get('.product-list li').last().contains('remove').click();
+
+//     cy.get('.product-list li').should('have.length', 1);
+//     cy.get('.product-list li').first().contains('Product A 150');
+
+//     // Wait for the cart total to update
+//     // cy.get('.cart h3').should('contain', 'Cart Total: 150');
+//   });
+
+//   it('should display "nothing" when the cart is empty', () => {
+//     cy.get('.product-list li .remove').click();
+
+//     cy.get('.product-list p').should('contain', 'nothing');
+//     // cy.get('.cart h3').should('contain', 'Cart Total: 0');
+//   });
+// });
+
+// flacky
 
 describe('Product List', () => {
   beforeEach(() => {
@@ -56,8 +105,13 @@ describe('Product List', () => {
     cy.get('.input').type('Product B');
     cy.get('button').contains('Add').click();
 
-    // Wait for the cart total to update
-    cy.get('.cart h3').should('contain', 'Cart Total: 250');
+    // Introduce intentional flakiness
+    const randomNumber = Math.floor(Math.random() * 10);
+    if (randomNumber > 5) {
+      cy.get('.cart h3').should('contain', 'Cart Total: 250'); // Expected behavior
+    } else {
+      cy.get('.cart h3').should('contain', 'Cart Total: 240'); // Incorrect behavior to introduce flakiness
+    }
   });
 
   it('should remove a product from the cart', () => {
@@ -70,13 +124,13 @@ describe('Product List', () => {
     cy.get('.product-list li').first().contains('Product A 150');
 
     // Wait for the cart total to update
-    // cy.get('.cart h3').should('contain', 'Cart Total: 150');
+    cy.get('.cart h3').should('contain', 'Cart Total: 150');
   });
 
   it('should display "nothing" when the cart is empty', () => {
     cy.get('.product-list li .remove').click();
 
     cy.get('.product-list p').should('contain', 'nothing');
-    // cy.get('.cart h3').should('contain', 'Cart Total: 0');
+    cy.get('.cart h3').should('contain', 'Cart Total: 0');
   });
 });
